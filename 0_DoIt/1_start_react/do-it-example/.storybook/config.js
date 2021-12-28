999cc938cff69f0211664d4acec8e83139eb0651
binna <every5116@naver.com>
2021-12-28 19:48:57 +0900

    비주얼 테스트로 더 쉽게 개발하기

---
+++ 0_DoIt/1_start_react/do-it-example/.storybook/config.js
@@ -0,0 +1,16 @@
+import { configure, setAddon } from '@storybook/react';
+import interopRequireDefault from 'babel-runtime/helpers/interopRequireDefault';
+import JSXAddon from 'storybook-addon-jsx';
+
+function loadStories() {
+    // require('../src/stories/InputStory');
+    // require('../src/stories/NewCounterStory');
+    const context = require.context('../src/stories', true, /Story\.jsx$/);
+
+    context.keys().forEach(srcFile => {
+        interopRequireDefault(context(srcFile));
+    });
+}
+
+setAddon(JSXAddon);
+configure(loadStories, module);
